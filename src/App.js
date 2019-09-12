15ad884895d2cc2744febbcc9513fb252d1c9638
Alexandra Lim <alexandra.lim@gmail.com>
2019-09-11 22:13:58 -0400

    Added Header and Footer components, and Route/Links using React-Router-Dom :link:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// placeholder functional components
+const NewGame = () => {
+	return <h2>New Game here</h2>;
+};
+
+const SavedGame = () => {
+	return <h2>Saved Game here</h2>;
+};
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className='App'>
+					<Header />
+
+					{/* New Game and Saved Game Buttons */}
+					<div className='newGameButton'>
+						<Link to='/newGame'>New Game</Link>
+					</div>
+
+					<div className='savedGameButton'>
+						<Link to='/savedGame'>Saved Games</Link>
+					</div>
+
+
+					<Route path='/newGame' component={NewGame} />
+					<Route path='/savedGame' component={SavedGame} />
+
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Built in 2019 by <a href="https://www.alexandralim.dev" target="_blank">Alexandra Lim</a>, <a href="https://anamorales.dev/" target="_blank">Ana Morales</a>, <a href="https://www.danielfitz.com" target="_blank">Dan Fitz</a>, and <a href="http://" target="_blank">Puja Neupane</a></p>
+        </footer>
+    );
+}
+
+export default Footer;
