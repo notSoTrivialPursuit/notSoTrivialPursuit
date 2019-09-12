038550ab750572fd43b7554bc0aeabde07fe74f8
Alexandra Lim <alexandra.lim@gmail.com>
2019-09-11 22:38:23 -0400

    updated portfolio links

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>Built in 2019 by <a href="https://www.alexandralim.dev" target="_blank">Alexandra Lim</a>, <a href="https://anamorales.dev/" target="_blank">Ana Morales</a>, <a href="https://www.danielfitz.com" target="_blank">Dan Fitz</a>, and <a href="http://" target="_blank">Puja Neupane</a></p>
+            <p>Built in 2019 by <a href="https://www.alexandralim.dev" target="_blank">Alexandra Lim</a>, <a href="https://anamorales.dev/" target="_blank">Ana Morales</a>, <a href="https://www.danielfitz.com" target="_blank">Dan Fitz</a>, and <a href="http://http://www.pujacodes.com/" target="_blank">Puja Neupane</a></p>
         </footer>
     );
 }
